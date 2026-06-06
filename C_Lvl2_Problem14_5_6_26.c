51d4b59b60ac9ae56b9ad65b382c41349cca4321
vasanth-arulnathan <vasantharulnathan2005@gmail.com>
2026-06-06 23:03:49 +0530

    C Level 2 - Problem 13,14

--- C_Lvl2_Problem14_5_6_26.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int x,y;
-    printf("Enter Number:");
-    scanf("%d", &x);
-    
-    printf("Result=%d",y);
-}
---
+++ C_Lvl2_Problem14_6_6_26.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter Number : ");
+    scanf("%d",&x);
+    y = (x / 100) == (x % 100);
+    printf("Result = %d",y);
+}
